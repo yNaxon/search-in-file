9cd48031a6381c17574bf4c44462173657519fba
Yonatan Naxon <yonatan@rethinkedge.com>
2021-10-16 15:45:22 +0300

    add logger middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,27 @@
+import { Middleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit'
 import textSearchReducer from '../features/text-search/text-search-slice';
+
+
+const logger: Middleware= store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action)
+
+  const result = next(action);
+  
+  console.info('next state', store.getState());
+  console.groupEnd();
+
+  return result
+}
 
 const store = configureStore({
   reducer: {
     textSearch: textSearchReducer,
   },
+  middleware: [logger]
 });
+
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
